b7abd34197e310b75021ac0fa0c159dc6869ee17
raulnicolasagusto <raulnicolasagusto@gmail.com>
2024-06-02 12:32:22 -0300

    gastos chart

--- index.php
+++ index.php
@@ -33,6 +33,9 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.6.9/sweetalert2.min.css">
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 
+  <!-- Google Font: Source Sans Pro para charts -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+
   <style>
         /* Estilo personalizado para el div */
         #miDiv {
@@ -170,6 +173,105 @@
       "responsive": true,
     });
   });
+</script>
+<script>
+
+      //GRAFICOS-------------
+    //- DONUT CHART -
+    //-------------
+    // Get context with jQuery - using jQuery's .get() method.
+    var donutChartCanvas = $('#donutChart').get(0).getContext('2d')
+    var donutData        = {
+      labels: [
+          'Chrome',
+          'IE',
+          'FireFox',
+          'Safari',
+          'Opera',
+          'Navigator',
+      ],
+      datasets: [
+        {
+          data: [700,500,400,600,300,100],
+          backgroundColor : ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc', '#d2d6de'],
+        }
+      ]
+    }
+    var donutOptions     = {
+      maintainAspectRatio : false,
+      responsive : true,
+    }
+    //Create pie or douhnut chart
+    // You can switch between pie and douhnut using the method below.
+    new Chart(donutChartCanvas, {
+      type: 'doughnut',
+      data: donutData,
+      options: donutOptions
+    })
+
+
+ //- AREA CHART -
+    //--------------
+
+    // Get context with jQuery - using jQuery's .get() method.
+    var areaChartCanvas = $('#areaChart').get(0).getContext('2d')
+
+    var areaChartData = {
+      labels  : ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label               : 'Digital Goods',
+          backgroundColor     : 'rgba(60,141,188,0.9)',
+          borderColor         : 'rgba(60,141,188,0.8)',
+          pointRadius          : false,
+          pointColor          : '#3b8bba',
+          pointStrokeColor    : 'rgba(60,141,188,1)',
+          pointHighlightFill  : '#fff',
+          pointHighlightStroke: 'rgba(60,141,188,1)',
+          data                : [28, 48, 40, 19, 86, 27, 90]
+        },
+        {
+          label               : 'Electronics',
+          backgroundColor     : 'rgba(210, 214, 222, 1)',
+          borderColor         : 'rgba(210, 214, 222, 1)',
+          pointRadius         : false,
+          pointColor          : 'rgba(210, 214, 222, 1)',
+          pointStrokeColor    : '#c1c7d1',
+          pointHighlightFill  : '#fff',
+          pointHighlightStroke: 'rgba(220,220,220,1)',
+          data                : [65, 59, 80, 81, 56, 55, 40]
+        },
+      ]
+    }
+
+    var areaChartOptions = {
+      maintainAspectRatio : false,
+      responsive : true,
+      legend: {
+        display: false
+      },
+      scales: {
+        xAxes: [{
+          gridLines : {
+            display : false,
+          }
+        }],
+        yAxes: [{
+          gridLines : {
+            display : false,
+          }
+        }]
+      }
+    }
+
+    // This will get the first returned node in the jQuery collection.
+    new Chart(areaChartCanvas, {
+      type: 'line',
+      data: areaChartData,
+      options: areaChartOptions
+    })
+
+
 </script>
 </body>
 </html>
--- pages/gastosUsuario.php
+++ pages/gastosUsuario.php
@@ -1,5 +1,79 @@
+
+
+
+
  <!-- Main content -->
- <div class="content-wrapper">
+<div class="content-wrapper">
+<!-- charts -->
+<section class="content">
+
+  <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-6">
+              <!-- AREA CHART -->
+
+              <!-- /.card -->
+
+              <!-- DONUT CHART -->
+              <div class="card card-danger">
+                <div class="card-header">
+                  <h3 class="card-title">Donut Chart</h3>
+
+                  <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                      <i class="fas fa-minus"></i>
+                    </button>
+                    <button type="button" class="btn btn-tool" data-card-widget="remove">
+                      <i class="fas fa-times"></i>
+                    </button>
+                  </div>
+                </div>
+                <div class="card-body">
+                  <canvas id="donutChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                </div>
+                <!-- /.card-body -->
+              </div>
+              <!-- /.card -->
+              <!-- AREA CHART -->
+
+
+              <!-- /.card -->
+
+            </div>
+            <!-- /.col (LEFT) -->
+            <div class="col-md-6">
+
+            <div class="card card-primary">
+                <div class="card-header">
+                  <h3 class="card-title">Area Chart</h3>
+
+                  <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                      <i class="fas fa-minus"></i>
+                    </button>
+                    <button type="button" class="btn btn-tool" data-card-widget="remove">
+                      <i class="fas fa-times"></i>
+                    </button>
+                  </div>
+                </div>
+                <div class="card-body">
+                  <div class="chart">
+                    <canvas id="areaChart" style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                  </div>
+                </div>
+                <!-- /.card-body -->
+              </div>
+
+            </div>
+            <!-- /.col (RIGHT) -->
+          </div>
+          <!-- /.row -->
+  </div>
+
+</section>
+
+<!-- fin charts -->
+
  <section class="content">
       <div class="container-fluid">
         <div class="row">
@@ -17,6 +91,7 @@
 
                   <th scope="col">Gasto/Compra</th>
                   <th scope="col">Monto</th>
+                  <th scope="col">Fecha</th>
                   <th scope="col">Cuota</th>
                   <th scope="col">Tipo de Gasto</th>
                   <th scope="col">Compartido</th>
@@ -28,6 +103,7 @@
                 <tr>
                   <td>Alquiler</td>
                   <td>$290000</td>
+                  <td>01/05/2024</td>
                   <td>no</td>
                   <td>Fijo</td>
                   <td>si</td>
@@ -37,6 +113,7 @@
                 <tr>
                  <td>Crema Rishis</td>
                   <td>$45000</td>
+                  <td>01/05/2024</td>
                   <td>1/4</td>
                   <td>Variable</td>
                   <td>no</td>
@@ -46,6 +123,7 @@
                 <tr>
                  <td>Mercaderia Carrefour</td>
                   <td>$160000</td>
+                  <td>01/05/2024</td>
                   <td>1/3</td>
                   <td>Variable</td>
                   <td>si</td>
@@ -63,7 +141,11 @@
             </div>
             </div>
  <!-- FIN Main content -->
-<!--
+
+
+
+
+ <!--
   <div class="content-wrapper">
     <div class="card">
         <div id="miDiv" class="text-center">
